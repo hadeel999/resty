d6e73e5c5fe1a1208a38a25455b55cff386978da
hadeel999 <hadeelahmadsaleh@gmail.com>
2022-08-09 00:13:47 +0300

    resty done

---
+++ src/components/header/header.js
@@ -0,0 +1,18 @@
+import './header.scss';
+const Header = function () {
+    return (
+      <>
+        <header>
+        <h1>RESTy</h1>
+        <nav>
+          <a href="/"> Home </a>
+          <a href="/"> History</a>
+          <a href="/"> Help</a>
+        </nav>
+
+      </header>
+      </>
+    );
+  };
+  
+  export default Header;
